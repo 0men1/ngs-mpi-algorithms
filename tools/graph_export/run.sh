2dca53f119782abe9cc574a57286280115b38fe2
0men1 <homahomen2@gmail.com>
2026-03-21 10:44:04 -0500

    raw ngs graph gen

---
+++ tools/graph_export/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+config_file=${1:-configs/defconfig.conf}
+output_file=${2:-outputs/graph.json}
+
+# Generate the graph using NGS
+ngs_graph_file="raw_ngs_graph"
+java -Xms2G -Xmx30G -Dconfig.file=$config_file -jar NetGameSim/target/scala-3.2.2/netmodelsim.jar $ngs_graph_file
+
+
+# Run graph enrichment
+
+
